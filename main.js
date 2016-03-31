c69170196422194b2511afc33812b0013b0c2035
BradySmith6357 <bsmit6357@gmail.com>
2016-03-30 20:22:47 -0600

    added a photo style instead of text code

--- main.js
+++ main.js
@@ -67,4 +67,39 @@ angular.module("mainApp")
 		$scope.randomCode = ""
 		}
 
+// ****** Using Images **********
+
+		$scope.images = [
+			{
+				img: "https://img1.etsystatic.com/063/0/10054863/il_fullxfull.760639189_i6jg.jpg",
+				answer: "9296"
+			},
+			{
+				img: "https://img0.etsystatic.com/025/0/6328095/il_fullxfull.602693560_diuj.jpg",
+				answer: "440"
+			},
+			{
+				img: "http://img2-1.timeinc.net/toh/i/g/14/products/01-house-numbers/02-house-number.jpg",
+				answer: "701"
+			}
+		]
+
+		$scope.randomImg = {}
+
+		$scope.genImage = function(){
+			$scope.randomIndex = Math.floor((Math.random() * $scope.images.length))
+			$scope.randomImg = $scope.images[$scope.randomIndex]
+			console.log($scope.randomImg)
+		}
+
+		$scope.checkImg = function(){
+			$scope.success2 = false
+			$scope.fail2 = false
+			if($scope.randomImg.answer == $scope.imgAnswer) {
+				$scope.success2 = true
+			} else {
+				$scope.fail2 = true
+			}
+		}
+
 		}])
